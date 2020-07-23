574241a333b2e15a365ddc205419daef6f97853b
Robin Norwood <rnorwood@hashicorp.com>
2020-07-23 11:12:13 -0500

    Initial configuration for count tutorial

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output public_dns_name {
+  description = "Public DNS name of load balancer"
+  value       = module.elb_http.this_elb_dns_name
+}
+
+output vpc_arn {
+  description = "ARN of the vpc"
+  value       = module.vpc.vpc_arn
+}
+
+output instance_ids {
+  description = "IDs of EC2 instances"
+  value       = [aws_instance.app_a.id, aws_instance.app_b.id]
+}
---
+++ variables.tf
@@ -0,0 +1,87 @@
+variable aws_region {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable project_name {
+  description = "Name of the project. Used in resource names and tags."
+  type        = string
+  default     = "client-webapp"
+}
+
+variable environment {
+  description = "Value of the 'Environment' tag."
+  type        = string
+  default     = "dev"
+}
+
+variable public_subnet_count {
+  description = "Number of public subnets. Maximum of 16."
+  type        = number
+  default     = 2
+}
+
+variable private_subnet_count {
+  description = "Number of private subnets. Maximum of 16."
+  type        = number
+  default     = 2
+}
+
+variable instance_type {
+  description = "Type of EC2 instance to use."
+  type        = string
+  default     = "t2.micro"
+}
+
+variable vpc_cidr_block {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable public_subnet_cidr_blocks {
+  description = "Available cidr blocks for public subnets"
+  type        = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24",
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24",
+    "10.0.7.0/24",
+    "10.0.8.0/24",
+    "10.0.9.0/24",
+    "10.0.10.0/24",
+    "10.0.11.0/24",
+    "10.0.12.0/24",
+    "10.0.13.0/24",
+    "10.0.14.0/24",
+    "10.0.15.0/24",
+    "10.0.16.0/24"
+  ]
+}
+
+variable private_subnet_cidr_blocks {
+  description = "Available cidr blocks for private subnets"
+  type        = list(string)
+  default = [
+    "10.0.101.0/24",
+    "10.0.102.0/24",
+    "10.0.103.0/24",
+    "10.0.104.0/24",
+    "10.0.105.0/24",
+    "10.0.106.0/24",
+    "10.0.107.0/24",
+    "10.0.108.0/24",
+    "10.0.109.0/24",
+    "10.0.110.0/24",
+    "10.0.111.0/24",
+    "10.0.112.0/24",
+    "10.0.113.0/24",
+    "10.0.114.0/24",
+    "10.0.115.0/24",
+    "10.0.116.0/24"
+  ]
+}
