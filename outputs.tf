1ab349c953ad734deb866d4db0619bfae94331ba
Robin Norwood <rnorwood@hashicorp.com>
2021-01-21 12:06:35 -0600

    Double quote variable and output names

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output public_dns_names {
+output "public_dns_names" {
   description = "Public DNS names of the load balancers for each project"
   value       = { for p in sort(keys(var.project)) : p => module.elb_http[p].this_elb_dns_name }
 }
