3a95933094a9400563aafec8524dea311dd8ad24
Tu Nguyen <im2nguyen@users.noreply.github.com>
2021-05-28 11:17:37 -0700

    add amazon-linux-extras before installing httpd

--- main.tf
+++ main.tf
@@ -111,6 +111,8 @@ resource "aws_instance" "app" {
   user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y
+    sudo yum install -y amazon-linux-extras
+    sudo amazon-linux-extras enable httpd_modules
     sudo yum install httpd -y
     sudo systemctl enable httpd
     sudo systemctl start httpd
