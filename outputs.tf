2f36437fd15d11997f337b6bce0e8bfe703a0090
Robin Norwood <rnorwood@hashicorp.com>
2022-06-02 12:39:41 -0500

    Updates for new modules

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output public_dns_name {
+output "public_dns_name" {
   description = "Public DNS name of load balancer"
-  value       = module.elb_http.this_elb_dns_name
+  value       = module.elb_http.elb_dns_name
 }
 
-output vpc_arn {
+output "vpc_arn" {
   description = "ARN of the vpc"
   value       = module.vpc.vpc_arn
 }
 
-output instance_ids {
+output "instance_ids" {
   description = "IDs of EC2 instances"
   value       = aws_instance.app.*.id
 }
