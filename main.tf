cc23cde4d7bde175f2f15c367b57d4f46295a4b6
Robin Norwood <rnorwood@hashicorp.com>
2022-06-10 15:11:27 -0500

    Update ec2 instance

--- main.tf
+++ main.tf
@@ -108,8 +108,6 @@ resource "aws_instance" "app" {
   user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y
-    sudo yum install -y amazon-linux-extras
-    sudo amazon-linux-extras enable httpd_modules
     sudo yum install httpd -y
     sudo systemctl enable httpd
     sudo systemctl start httpd
